8c26e9f6d9e1eb6b6db7c86167d5844247feea54
Viacheslav Kozlov <slavick.cv@gmail.com>
2021-09-22 21:58:46 +0300

    tasks1-5

---
+++ practice/src/Components/header/headerList/HeaderList.js
@@ -0,0 +1,12 @@
+import data from '../../../data/index'
+
+const HeaderList = () => {
+    return (
+        <ul>
+            {data.header.map((headerItem) => (
+                <li key={headerItem} > {headerItem} </li>
+            ))}
+        </ul>
+    );
+};
+export default HeaderList
---
+++ practice/src/Components/main/Main.js
@@ -0,0 +1,13 @@
+import React from "react";
+import data from "../../data";
+import PhoneList from "../phoneList/PhoneList";
+
+const Main = () => {
+    return (
+        <>
+            <PhoneList phones={data.phones} />
+        </>
+    );
+};
+
+export default Main;
